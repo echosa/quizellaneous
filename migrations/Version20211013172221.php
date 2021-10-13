298f86a478b24c654e7cd16837157eea961cf0a9
Brian Zwahr <echosa@echosa.net>
2021-10-13 12:23:48 -0500

    Redo initial migration

---
+++ migrations/Version20211013172221.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20211013172221 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE classical_music_composer (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, name VARCHAR(255) NOT NULL, birth_date DATETIME NOT NULL, death_date DATETIME DEFAULT NULL, slug VARCHAR(255) NOT NULL)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE classical_music_composer');
+    }
+}
--- src/Repository/ClassicalMusicComposerRepository.php
+++ src/Repository/ClassicalMusicComposerRepository.php
@@ -18,33 +18,4 @@ class ClassicalMusicComposerRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, ClassicalMusicComposer::class);
     }
-
-    // /**
-    //  * @return ClassicalMusicComposer[] Returns an array of ClassicalMusicComposer objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('c')
-            ->andWhere('c.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('c.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?ClassicalMusicComposer
-    {
-        return $this->createQueryBuilder('c')
-            ->andWhere('c.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
