e2107960c682b4e459ea73cd1c1ecd9c46af62bd
Brian Zwahr <echosa@echosa.net>
2021-10-13 15:47:06 -0500

    Use fixtures to test data

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,8 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\ClassicalMusicComposer;
+use DateTime;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,8 +11,19 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $bach = new ClassicalMusicComposer();
+        $bach->setName('Johann Sebastian Bach');
+        $bach->setBirthDate(new DateTime('1685-03-31'));
+        $bach->setDeathDate(new DateTime('1750-07-28'));
+        $bach->setSlug('johann-sebastian-bach');
+        $manager->persist($bach);
+
+        $mozart = new ClassicalMusicComposer();
+        $mozart->setName('Wolfgang Amadeus Mozart');
+        $mozart->setBirthDate(new DateTime('1756-01-27'));
+        $mozart->setDeathDate(new DateTime('1791-12-05'));
+        $mozart->setSlug('wolfgang-amadeus-mozart');
+        $manager->persist($mozart);
 
         $manager->flush();
     }
