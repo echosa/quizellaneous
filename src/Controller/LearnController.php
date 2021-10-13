6886fbff6b9a26b709c1d942b0fea0dd44d304bc
Brian Zwahr <echosa@echosa.net>
2021-10-13 16:09:25 -0500

    Add category browsing

--- src/Controller/LearnController.php
+++ src/Controller/LearnController.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Entity\Category;
 use App\Entity\ClassicalMusicComposer;
+use App\Repository\CategoryRepository;
 use App\Repository\ClassicalMusicComposerRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -15,11 +17,25 @@ class LearnController extends AbstractController
     /**
      * @Route("/learn", name="learn")
      */
-    public function learn(ClassicalMusicComposerRepository $classicalMusicComposerRepository): Response
+    public function learn(CategoryRepository $categoryRepository): Response
     {
-        $composers = $classicalMusicComposerRepository->findAll();
+        $categories = $categoryRepository->findAll();
 
-        return $this->render('learn.html.twig', ['composers' => $composers]);
+        return $this->render('learn.html.twig', ['categories' => $categories]);
+    }
+
+    /**
+     * @Route("/learn/category/{slug}", name="learn_category")
+     */
+    public function learnCategory(Category $category, ClassicalMusicComposerRepository $classicalMusicComposerRepository): Response
+    {
+        if ($category->getSlug() === 'classical-music') {
+            $subjects = $classicalMusicComposerRepository->findAll();
+        } else {
+            throw $this->createNotFoundException();
+        }
+
+        return $this->render('learn_category.html.twig', ['category' => $category, 'subjects' => $subjects]);
     }
 
     /**
