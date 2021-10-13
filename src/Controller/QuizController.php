d91d197b058717e1411283c379c1da679459ca8d
Brian Zwahr <echosa@echosa.net>
2021-10-13 14:07:25 -0500

    Move questions into Symfony form

--- src/Controller/QuizController.php
+++ src/Controller/QuizController.php
@@ -4,8 +4,11 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Model\Question;
 use App\Provider\QuestionsProvider;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -18,6 +21,31 @@ class QuizController extends AbstractController
     {
         $questions = $questionsProvider->getQuestions();
 
-        return $this->render('quiz.html.twig', ['questions' => $questions]);
+        $count = 1;
+        $formBuilder = $this->createFormBuilder();
+        /** @var Question $question */
+        foreach ($questions as $question) {
+            $formBuilder->add(
+                sprintf('question-%s', $count),
+                ChoiceType::class,
+                [
+                    'mapped' => false,
+                    'expanded' => true,
+                    'multiple' => false,
+                    'label' => $question->getQuestion(),
+                    'choices' => $question->getChoices(),
+                    'choice_label' => function ($choice, $key, $value) {
+                        return $value;
+                    },
+                ]
+            );
+            ++$count;
+        }
+
+        $form = $formBuilder
+            ->add('save', SubmitType::class, ['label' => 'Check Answers'])
+            ->getForm();
+
+        return $this->render('quiz.html.twig', ['questions' => $questions, 'form' => $form->createView()]);
     }
 }
